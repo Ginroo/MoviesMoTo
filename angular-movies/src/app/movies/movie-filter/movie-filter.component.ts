a7fe3c07f9bb732eb56da6dfeb20d57f29277546
Rogin Roi Reillo <reillorrs@outlook.com>
2021-05-27 23:24:10 +0800

    Some changes

--- angular-movies/src/app/movies/movie-filter/movie-filter.component.ts
+++ angular-movies/src/app/movies/movie-filter/movie-filter.component.ts
@@ -11,6 +11,9 @@ export class MovieFilterComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
 
   form!: FormGroup;
+
+  genres = [{id: 1, name: 'Drama'}, {id: 2, name: 'Action'}];
+
   ngOnInit(): void {
     this.form =this.formBuilder.group({
       title: '',
@@ -19,5 +22,11 @@ export class MovieFilterComponent implements OnInit {
       inTheaters: false
     })
   }
+
+  //#region clear form
+  clearForm(){
+
+  }
+  //#endregion
 
 }
