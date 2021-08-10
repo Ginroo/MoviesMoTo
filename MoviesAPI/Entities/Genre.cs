cd5f77fe1c9fa4bca3c4ef57197f52ac15e333b0
Rogin Roi Reillo <reillo131@gmail.com>
2021-08-10 22:01:53 +0800

    Add repository in backend

---
+++ MoviesAPI/Entities/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Entities
+{
+    public class Genre
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
